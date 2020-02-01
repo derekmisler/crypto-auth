4ef8ecb94529a9769c01aa46605e7fc214a9679d
Derek Misler <derekmisler@gmail.com>
2020-02-01 14:39:41 -0500

    extensions

--- preact.config.js
+++ preact.config.js
@@ -4,11 +4,13 @@ export default function(config, env, helpers) {
   delete config.node
   // dotenv injection
   const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0]
-  Object.assign(
-    plugin.definitions,
-    Object.keys(parsed || {}).reduce((env, key) => ({
+  plugin.definitions = {
+    ...plugin.definitions,
+    ...Object.keys(parsed || {}).reduce((env, key) => ({
       ...env,
       [`process.env.${key}`]: JSON.stringify(parsed[key])
     }), {})
-  )
+  }
+  config.resolve.extensions.push('.jsx')
+  return config
 }
